fd3a9177e979ca333cc540b579a76e3ecc2865dc
Gabriel <gabrielnetbrine@gmail.com>
2023-08-31 20:52:15 -0300

    ✨ feat: alterando alguns metodos e atributos

--- src/Livro.java
+++ src/Livro.java
@@ -7,9 +7,9 @@ public class Livro {
     private int categoria;
     private boolean disponibilidade;
     private String localizacao;
+    private int ID;
 
-    public void setInfos(String titulo, String autor, String editora, int ISBN, int categoria, boolean disponibilidade, String localizacao) {
-
+    public Livro(String titulo, String autor, String editora, int ISBN, int categoria, boolean disponibilidade, String localizacao) {
         this.titulo = titulo;
         this.autor = autor;
         this.editora = editora;
@@ -17,35 +17,61 @@ public class Livro {
         this.categoria = categoria;
         this.disponibilidade = disponibilidade;
         this.localizacao = localizacao;
-
     }
 
     public String getTitulo() {
         return titulo;
     }
 
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
     public String getAutor() {
         return autor;
     }
 
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
 
     public String getEditora() {
         return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
     }
 
     public int getISBN() {
         return ISBN;
     }
 
+    public void setISBN(int ISBN) {
+        this.ISBN = ISBN;
+    }
+
     public int getCategoria() {
         return categoria;
     }
 
-    public boolean isDisponibilidade() {
+    public void setCategoria(int categoria) {
+        this.categoria = categoria;
+    }
+
+    public boolean getDisponibilidade() {
         return disponibilidade;
+    }
+
+    public void setDisponibilidade(boolean disponibilidade) {
+        this.disponibilidade = disponibilidade;
     }
 
     public String getLocalizacao() {
         return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
     }
 }
