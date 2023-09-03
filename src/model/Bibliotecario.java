740e97d7fde270dabb8a57afc1c1ddef89d9fdb3
Gabriel <gabrielnetbrine@gmail.com>
2023-09-03 12:41:12 -0300

    :sparkles: feat: adicionando a funcionalidade de registrar livros

--- src/model/Bibliotecario.java
+++ src/model/Bibliotecario.java
@@ -1,9 +1,16 @@
 package model;
 
+import DAO.acervo.AcervoDAOmap;
+
 public class Bibliotecario extends Pessoa {
 
-    public void registroLivro(){
-        ////
+    public Bibliotecario(String name, int ID) {
+        super(name, ID);
+    }
+
+    public void registroLivro(Livro livro){
+        AcervoDAOmap registro = new AcervoDAOmap();
+        registro.create(livro);
     }
 
     public void registroEmprestimo(){
