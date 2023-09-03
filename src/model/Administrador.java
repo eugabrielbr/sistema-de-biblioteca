fcd683cf34f78028f5f93608a14343b1fc0dcd86
Gabriel <gabrielnetbrine@gmail.com>
2023-09-03 12:40:30 -0300

    :sparkles: feat: criando construtores super

--- src/model/Administrador.java
+++ src/model/Administrador.java
@@ -1,10 +1,15 @@
 package model;
 
-public class Administrador{
+public class Administrador extends Pessoa{
 
 
     private int cargo;
     private String Senha;
+
+    public Administrador(String name, int ID) {
+        super(name, ID);
+    }
+
     public void cadastroNovoUsuario(){
         ///
     }
---
+++ src/model/Pesquisa.java
@@ -0,0 +1,9 @@
+package model;
+public class Pesquisa extends Livro {
+
+
+    public Pesquisa(String titulo, String autor, String editora, int ISBN, String categoria, boolean disponibilidade, String localizacao) {
+        super(titulo, autor, editora, ISBN, categoria, disponibilidade, localizacao);
+    }
+
+}
