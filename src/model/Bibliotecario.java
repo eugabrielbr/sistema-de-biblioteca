e8ed7ed8c58280c739ab070fd0ca268fb74bae61
Gabriel <gabrielnetbrine@gmail.com>
2023-09-07 16:07:07 -0300

    :recycle:a refactor: removendo alguns metodos

--- src/model/Bibliotecario.java
+++ src/model/Bibliotecario.java
@@ -1,23 +1,10 @@
 package model;
 
-import DAO.acervo.AcervoDAOmap;
-
 public class Bibliotecario extends Pessoa {
 
     public Bibliotecario(String name, int ID) {
         super(name, ID);
     }
-
-    public void registroLivro(Livro livro){
-        AcervoDAOmap registro = new AcervoDAOmap();
-        registro.create(livro);
-    }
-
-    public void registroEmprestimo(){
-
-        ////
-    }
-
 
 }
 
