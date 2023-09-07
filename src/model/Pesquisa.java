2a60c43f78c9e95c771b23118fd5fa2375c97360
Gabriel <gabrielnetbrine@gmail.com>
2023-09-07 16:03:16 -0300

    :fire: deletando arquivo

--- src/model/Pesquisa.java
+++
@@ -1,9 +0,0 @@
-package model;
-public class Pesquisa extends Livro {
-
-
-    public Pesquisa(String titulo, String autor, String editora, int ISBN, String categoria, boolean disponibilidade, String localizacao) {
-        super(titulo, autor, editora, ISBN, categoria, disponibilidade, localizacao);
-    }
-
-}
