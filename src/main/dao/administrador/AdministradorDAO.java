5c9655f681e213ce9f56e9a4e2ba447184b75ec0
Gabriel <gabrielnetbrine@gmail.com>
2023-09-16 18:32:14 -0300

    :sparkles: feat: adicionando DAO do administrador

---
+++ src/main/dao/administrador/AdministradorDAO.java
@@ -0,0 +1,7 @@
+package main.dao.administrador;
+
+import main.dao.CRUD;
+import main.model.Administrador;
+
+public interface AdministradorDAO extends CRUD<Administrador> {
+}
---
+++ src/main/dao/administrador/AdministradorDAOmap.java
@@ -0,0 +1,4 @@
+package main.dao.administrador;
+
+public class AdministradorDAOmap {
+}
