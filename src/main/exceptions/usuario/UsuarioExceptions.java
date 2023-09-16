eaa03bebdfbc9a1d0914a612a8fbef4a3b71bbca
Gabriel <gabrielnetbrine@gmail.com>
2023-09-16 18:50:44 -0300

    :recycle: refactor: estendendo da classe LivroExcpetions para nao repetir codigo
    errei fui mlk

--- src/main/exceptions/usuario/UsuarioExceptions.java
+++ src/main/exceptions/usuario/UsuarioExceptions.java
@@ -1,15 +1,11 @@
 package main.exceptions.usuario;
 
-public class UsuarioExceptions extends Exception {
+import main.exceptions.livro.LivroExceptions;
 
-    private Integer ID;
-    public static final String DELETE = "Nao foi possivel deletar";
-    public static final String UPDATE = "Nao foi possivel atualizar";
-    public static final String NOT_FOUND = "Nao foi possivel encontrar o(s) objeto(s)";
+public class UsuarioExceptions extends LivroExceptions {
 
 
-    public UsuarioExceptions(String message, Integer ID) {
-        super(message + " - ID: " + ID);
-        this.ID = ID;
+    public UsuarioExceptions( String message, Integer ID ) {
+        super(message, ID);
     }
 }
