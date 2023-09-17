51119e9f66168ea19feaca9ee9e25c6cd7f9eab0
Gabriel <gabrielnetbrine@gmail.com>
2023-09-17 19:01:35 -0300

    :sparkles: feat: adicionando DAO, para o padrao Singleton
    assim, independente das instancias, sera possivel lidar com apenas um endereço de cada dao, evitando assim lidar com mais de um objeto de um mesmo DAO.

---
+++ src/main/dao/DAO.java
@@ -0,0 +1,53 @@
+package main.dao;
+
+import main.dao.administrador.AdministradorDAO;
+import main.dao.administrador.AdministradorDAOmap;
+import main.dao.emprestimo.EmprestimoDAO;
+import main.dao.emprestimo.EmprestimoDAOmap;
+import main.dao.livro.LivroDAOmap;
+import main.dao.usuario.UsuarioDAO;
+import main.dao.livro.LivroDAO;
+import main.dao.usuario.UsuarioDAOmap;
+
+public class DAO {
+
+
+    private static LivroDAO livroDAO;
+    private static UsuarioDAO usuarioDAO;
+    private static AdministradorDAO administradorDAO;
+    private static EmprestimoDAO emprestimoDAO;
+
+    public static LivroDAO getLivroDAO() {
+        if (livroDAO == null){
+            livroDAO = new LivroDAOmap();
+        }
+        return livroDAO;
+    }
+
+    public static UsuarioDAO getUsuarioDAO() {
+
+        if (usuarioDAO == null){
+            usuarioDAO = new UsuarioDAOmap();
+
+        }
+        return usuarioDAO;
+
+    }
+
+    public AdministradorDAO getAdministradorDAO() {
+
+        if (administradorDAO == null){
+            administradorDAO = new AdministradorDAOmap();
+        }
+
+        return administradorDAO;
+    }
+
+    public EmprestimoDAO getEmprestimoDAO() {
+
+        if (emprestimoDAO == null) {
+            emprestimoDAO = new EmprestimoDAOmap();
+        }
+        return emprestimoDAO;
+    }
+}
