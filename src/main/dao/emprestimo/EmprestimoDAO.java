4b70d6433dff73fbf6140eea1c117bcb5ab90d23
Gabriel <gabrielnetbrine@gmail.com>
2023-09-20 18:57:58 -0300

    :sparkles: feat: lista para buscar emprestimos do usuario pelo ID

--- src/main/dao/emprestimo/EmprestimoDAO.java
+++ src/main/dao/emprestimo/EmprestimoDAO.java
@@ -3,5 +3,8 @@ package main.dao.emprestimo;
 import main.dao.CRUD;
 import main.model.Emprestimo;
 
+import java.util.List;
+
 public interface EmprestimoDAO extends CRUD<Emprestimo> {
+    List<Emprestimo> findByUser(Integer IDuser);
 }
