5506286cde9a379fd172cf9d7bab3668e75ddfad
Gabriel <gabrielnetbrine@gmail.com>
2023-09-22 11:17:51 -0300

    :sparkles: feat: criando classes de testes para alguns DAOs

---
+++ src/test/DAO/AdministradorDAOmapTest.java
@@ -0,0 +1,32 @@
+package test.DAO;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdministradorDAOmapTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteMany() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void findMany() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
---
+++ src/test/DAO/EmprestimoDAOmapTest.java
@@ -0,0 +1,32 @@
+package test.DAO;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmprestimoDAOmapTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteMany() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void findMany() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
