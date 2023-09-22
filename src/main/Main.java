13dc747393c84812407f21c129e5412b7f369b91
Gabriel <gabrielnetbrine@gmail.com>
2023-09-22 11:18:46 -0300

    create main.java

---
+++ src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import main.dao.usuario.UsuarioDAO;
+import main.dao.usuario.UsuarioDAOmap;
+
+
+
+
+import java.util.*;
+
+public class Main {
+
+    public static void main( String[] args ) {
+
+
+        }
+
+
+
+    }
