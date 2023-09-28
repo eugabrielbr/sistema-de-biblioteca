aa0de632eaeda2f3712019ec9ffc6ca1acd26143
Gabriel <gabrielnetbrine@gmail.com>
2023-09-28 20:51:12 -0300

    :sparkles: feat: adicionando funcionalidades para relatorio
    - quantidade de livros atrasados
    - quantidade de livros reservados
    - historico de emprestimo de usuario especifico

--- src/main/model/func/AdmUseCases.java
+++ src/main/model/func/AdmUseCases.java
@@ -4,11 +4,14 @@ import main.dao.DAO;
 import main.dao.emprestimo.EmprestimoDAO;
 import main.dao.livro.LivroDAO;
 import main.dao.usuario.UsuarioDAO;
-import main.exceptions.crud.CrudExceptions;
+import main.exceptions.crud.DAOExceptions;
 
 import main.model.Emprestimo;
+import main.model.Livro;
 import main.model.Usuario;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Map.Entry;
 
 public class AdmUseCases {
@@ -27,7 +30,43 @@ public class AdmUseCases {
         return quant;
     }
 
-    public void bloquearDesbloquearUsuario( Integer ID, Integer option) throws CrudExceptions {
+    public Integer qtdLivrosAtrasados(){
+
+        Integer quant = 0;
+
+        for (Entry<Integer,Emprestimo> x : daoEmprestimo.findMany().entrySet()){
+
+            if (x.getValue().getDataDevolucao() != null && x.getValue().getDataDevolucao().isBefore(LocalDate.now())){
+                quant++;
+            }
+
+        }
+        return quant;
+
+    }
+
+    public Integer qtdLivrosReservados(){
+
+        Integer quant = 0;
+
+        for (Entry<Integer, Livro> x : daoLivro.findMany().entrySet()){
+
+            if (!x.getValue().getFilaReserva().isEmpty()){
+                quant++;
+            }
+
+        }
+        return quant;
+
+    }
+
+    public List<Emprestimo> emprestimoUsuario(Integer usuario) throws DAOExceptions {
+
+        List<Emprestimo> lista = daoUsuario.findById(usuario).getHistorico();
+        return lista;
+    }
+
+    public void bloquearDesbloquearUsuario( Integer ID, Integer option) throws DAOExceptions {
 
         //1 - bloquear
         //2 - desbloquear
