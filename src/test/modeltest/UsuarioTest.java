b2489d6da96096294782f833331b962bc0d7bed6
Gabriel <gabrielnetbrine@gmail.com>
2023-09-28 20:43:38 -0300

    :sparkles: feat: criando classe de teste para model usuario

---
+++ src/test/modeltest/UsuarioTest.java
@@ -0,0 +1,18 @@
+package test.modeltest;
+
+import org.junit.jupiter.api.Test;
+
+class UsuarioTest {
+
+    @Test
+    void registroHistoricoUser() {
+
+
+    }
+
+    @Test
+    void registrosLivrosDevolvidos() {
+    }
+
+
+}
