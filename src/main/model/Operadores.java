5ee181b394130eafe6280ffc00424ed5b31f5f4a
Gabriel <gabrielnetbrine@gmail.com>
2023-10-01 11:20:39 -0300

    :sparkles: feat: adicionando classe operadores

---
+++ src/main/model/Operadores.java
@@ -0,0 +1,12 @@
+package main.model;
+
+public class Operadores extends Pessoa {
+
+    private Integer cargo; //1 adm   2 biblio
+    private String senha;
+    private Integer IDdeAcesso;
+    public Operadores( String name ) {
+        super(name);
+    }
+
+}
