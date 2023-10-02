6abcd44983bf11b9ed3447154fccd90df673afda
Gabriel <gabrielnetbrine@gmail.com>
2023-10-02 15:01:58 -0300

    :recycle: refactor: construtores e getters e setters

--- src/main/model/Operadores.java
+++ src/main/model/Operadores.java
@@ -1,12 +1,49 @@
 package main.model;
 
+import java.util.Objects;
+
+/**
+ * Classe com atributos e metodos relacionados aos operadores
+ * @author Gabriel
+ */
 public class Operadores extends Pessoa {
 
+    /**
+     * cargo dos operadores
+     */
     private Integer cargo; //1 adm   2 biblio
+    /**
+     * senha dos operadores
+     */
     private String senha;
-    private Integer IDdeAcesso;
-    public Operadores( String name ) {
+
+    /**
+     * construtor da classe
+     * @param name nome do operador
+     * @param cargo cargo do operador
+     * @param senha senha do operador
+     */
+    public Operadores( String name, Integer cargo,String senha ) {
+
         super(name);
+        this.cargo = cargo;
+        this.senha = senha;
     }
 
+
+    public Integer getCargo() {
+        return cargo;
+    }
+
+    public void setCargo( Integer cargo ) {
+        this.cargo = cargo;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha( String senha ) {
+        this.senha = senha;
+    }
 }
