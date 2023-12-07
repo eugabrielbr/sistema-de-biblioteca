d817bed7ee9646ea7e6ecff6ad51c6255cafcdeb
Gabriel <gabrielnetbrine@gmail.com>
2023-12-07 17:36:55 -0300

    :sparkles: feat: criando instancias das classes DAOS para arquivos

--- src/main/dao/DAO.java
+++ src/main/dao/DAO.java
@@ -1,14 +1,20 @@
 package main.dao;
 
 
+import main.dao.emprestimo.ArquivoEmprestimoDAO;
 import main.dao.emprestimo.EmprestimoDAO;
 import main.dao.emprestimo.EmprestimoDAOmap;
+import main.dao.livro.ArquivoLivroDAO;
 import main.dao.livro.LivroDAOmap;
+import main.dao.operadores.ArquivoOperadoresDAO;
 import main.dao.operadores.OperadoresDAO;
 import main.dao.operadores.OperadoresDAOmap;
+import main.dao.usuario.ArquivoUsuarioDAO;
 import main.dao.usuario.UsuarioDAO;
 import main.dao.livro.LivroDAO;
 import main.dao.usuario.UsuarioDAOmap;
+
+import java.io.IOException;
 
 /**
  * classe Singleton do dao
@@ -16,6 +22,12 @@ import main.dao.usuario.UsuarioDAOmap;
  */
 public class DAO {
 
+    /**
+     * tipo de memoria que sera usada para armazenamento de dados
+     */
+    private static int typeOfStorage = 1;
+    //1 - MEMORIA
+    //* - ARQUIVO
     /**
      * livroDAO objeto LivroDAO
      */
@@ -37,9 +49,18 @@ public class DAO {
      * criar e retorna um objeto
      * @return objeto LivroDAOmap
      */
-    public static LivroDAO getLivroDAO() {
+
+    public static LivroDAO getLivroDAO() throws IOException, ClassNotFoundException {
+
         if (livroDAO == null){
-            livroDAO = new LivroDAOmap();
+
+            if(typeOfStorage == 1){
+                livroDAO = new LivroDAOmap();
+            }
+            else{
+                livroDAO = new ArquivoLivroDAO();
+            }
+
         }
         return livroDAO;
     }
@@ -47,10 +68,17 @@ public class DAO {
      * criar e retorna um objeto
      * @return objeto UsuarioDAOmap
      */
-    public static UsuarioDAO getUsuarioDAO() {
+    public static UsuarioDAO getUsuarioDAO() throws IOException, ClassNotFoundException {
 
         if (usuarioDAO == null){
-            usuarioDAO = new UsuarioDAOmap();
+
+            if (typeOfStorage == 1){
+                usuarioDAO = new UsuarioDAOmap();
+            }
+            else{
+                usuarioDAO = new ArquivoUsuarioDAO();
+            }
+
 
         }
         return usuarioDAO;
@@ -60,10 +88,17 @@ public class DAO {
      * criar e retorna um objeto
      * @return objeto OperadoresDAOmap
      */
-    public OperadoresDAO getOperadoresDAO() {
+    public static OperadoresDAO getOperadoresDAO() throws IOException, ClassNotFoundException {
 
         if (operadoresDAO == null){
-            operadoresDAO = new OperadoresDAOmap();
+
+            if (typeOfStorage == 1){
+                operadoresDAO = new OperadoresDAOmap();
+            }
+            else{
+                operadoresDAO = new ArquivoOperadoresDAO();
+            }
+
         }
 
         return operadoresDAO;
@@ -72,10 +107,16 @@ public class DAO {
      * criar e retorna um objeto
      * @return objeto EmprestimoDAOmap
      */
-    public static EmprestimoDAO getEmprestimoDAO() {
+    public static EmprestimoDAO getEmprestimoDAO() throws IOException, ClassNotFoundException {
 
         if (emprestimoDAO == null) {
-            emprestimoDAO = new EmprestimoDAOmap();
+
+            if(typeOfStorage == 1){
+                emprestimoDAO = new EmprestimoDAOmap();
+            }
+            else{
+                emprestimoDAO = new ArquivoEmprestimoDAO();
+            }
         }
         return emprestimoDAO;
     }
