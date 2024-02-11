724b2af3d3af14a03988f9f690c53dc70de6b7a7
Gabriel <gabrielnetbrine@gmail.com>
2024-02-10 22:00:34 -0300

    :sparkler: feat: adicionando tela de login e registro

--- src/main/java/com/example/sistemadebiblioteca/controller/LoginController.java
+++ src/main/java/com/example/sistemadebiblioteca/controller/LoginController.java
@@ -1,15 +1,22 @@
 package com.example.sistemadebiblioteca.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import com.example.sistemadebiblioteca.HelloApplication;
+import com.example.sistemadebiblioteca.dao.DAO;
+import com.example.sistemadebiblioteca.dao.operadores.OperadoresDAO;
+import com.example.sistemadebiblioteca.dao.usuario.UsuarioDAO;
+import com.example.sistemadebiblioteca.exceptions.dao.DAOExceptions;
+import com.example.sistemadebiblioteca.model.Operadores;
+import com.example.sistemadebiblioteca.model.Usuario;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+
 
 public class LoginController {
-
 
     @FXML
     private ResourceBundle resources;
@@ -18,25 +25,135 @@ public class LoginController {
     private URL location;
 
     @FXML
-    private Button botaoTeste;
+    private Button botaoLogin;
+
+    @FXML
+    private Button botaoVoltar;
+
+    @FXML
+    private Hyperlink hyperLinkRegistrar;
+
+    @FXML
+    private TextField loginTexto;
+
+    @FXML
+    private Label labelErro;
+
+    @FXML
+    private PasswordField senhaText;
+
+    private UsuarioDAO usuarioDAO;
+    private OperadoresDAO operadoresDAO;
+
+    public Operadores operador;
+
+    public Usuario usuario;
 
 
+    public LoginController() throws IOException, ClassNotFoundException {
+
+        this.usuarioDAO = DAO.getUsuarioDAO();
+        this.operadoresDAO = DAO.getOperadoresDAO();
+
+
+    }
+
+
+    @FXML
+    void botaoLoginAction(ActionEvent event) throws IOException, ClassNotFoundException, DAOExceptions {
+
+        this.labelErro.setText("");
+
+        try {
+
+            if (HelloController.isAdm || HelloController.isBib) {
+
+
+                Operadores operadorEncontrado = DAO.getOperadoresDAO().findById(Integer.parseInt(this.loginTexto.getText()));
+
+                if (operadorEncontrado.getSenha().equals(this.senhaText.getText())){
+
+                    this.operador = operadorEncontrado;
+
+                    if (operadorEncontrado.getCargo() == 1){
+                        //cena adm
+                    }
+                    else if(operadorEncontrado.getCargo() == 2){}
+                        //cena blib
+                }
+                else{
+                    this.labelErro.setText("Senha incorreta!");
+                }
+            }
+            else if (HelloController.isLei){
+
+                Usuario usuarioEncontrado = DAO.getUsuarioDAO().findById(Integer.parseInt(this.loginTexto.getText()));
+
+                if (usuarioEncontrado.getSenha().equals(this.senhaText.getText())){
+
+                    this.usuario = usuarioEncontrado;
+                    //aq onde passa cena
+                    System.out.println(usuarioEncontrado);
+                }
+                else{
+                    this.labelErro.setText("Senha incorreta!");
+                }
+            }
+        }
+        catch (Exception e){
+            this.labelErro.setText("Usuário não encontrado!");
+            this.senhaText.clear();
+            this.loginTexto.clear();
+        }
+    }
+
+    public void clear(){
+        this.senhaText.clear();
+        this.loginTexto.clear();
+        this.labelErro.setText("");
+    }
     @FXML
     void botaoVoltarAction(ActionEvent event) {
 
         HelloController.isAdm = false;
-        HelloController.isBib = false;
         HelloController.isLei = false;
+        HelloController.isBib = false;
+
+        clear();
 
         HelloApplication.changeScene("hello");
     }
 
+    @FXML
+    void hyperLinkRegistrarAction(ActionEvent event) {
 
+        clear();
+
+        HelloApplication.changeScene("registro");
+
+
+
+    }
+
+    @FXML
+    void loginTextoAction(ActionEvent event) {
+        //
+    }
+
+    @FXML
+    void senhaTextoAction(ActionEvent event) {
+        //
+    }
 
     @FXML
     void initialize() {
+        assert botaoLogin != null : "fx:id=\"botaoLogin\" was not injected: check your FXML file 'login-view.fxml'.";
+        assert botaoVoltar != null : "fx:id=\"botaoVoltar\" was not injected: check your FXML file 'login-view.fxml'.";
+        assert hyperLinkRegistrar != null : "fx:id=\"hyperLinkRegistrar\" was not injected: check your FXML file 'login-view.fxml'.";
+        assert labelErro != null : "fx:id=\"labelErro\" was not injected: check your FXML file 'login-view.fxml'.";
+        assert loginTexto != null : "fx:id=\"loginTexto\" was not injected: check your FXML file 'login-view.fxml'.";
+        assert senhaText != null : "fx:id=\"senhaText\" was not injected: check your FXML file 'login-view.fxml'.";
 
-        assert botaoTeste != null : "fx:id=\"botaoTeste\" was not injected: check your FXML file 'login-view.fxml'.";
 
     }
 
---
+++ src/main/java/com/example/sistemadebiblioteca/controller/RegistroController.java
@@ -0,0 +1,126 @@
+package com.example.sistemadebiblioteca.controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.example.sistemadebiblioteca.HelloApplication;
+import com.example.sistemadebiblioteca.dao.DAO;
+import com.example.sistemadebiblioteca.dao.usuario.UsuarioDAO;
+import com.example.sistemadebiblioteca.model.Usuario;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class RegistroController {
+
+    private UsuarioDAO usuarioDAO;
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label alertText;
+
+    @FXML
+    private Button botaoCadastro;
+
+    @FXML
+    private TextField enderecoText;
+
+    @FXML
+    private TextField nomeText;
+
+    @FXML
+    private TextField senhaRegistroText;
+
+    @FXML
+    private Label erroRegistro;
+
+    @FXML
+    private TextField teltext;
+
+    public void RegistroController() throws IOException, ClassNotFoundException {
+
+        this.usuarioDAO = DAO.getUsuarioDAO();
+    }
+
+    @FXML
+    private Button botaoVoltar;
+
+    @FXML
+    void botaoCadastroAction(ActionEvent event) throws IOException, ClassNotFoundException {
+
+        alertText.setText("");
+
+
+        String name = nomeText.getText();
+        String endereco = enderecoText.getText();
+        String telefone = teltext.getText();
+        String senha = senhaRegistroText.getText();
+
+
+        if (senha.length() <= 8){
+
+            if (!name.isEmpty() && !endereco.isEmpty() && !telefone.isEmpty() && !senha.isEmpty()) {
+
+                Usuario usuario = new Usuario(name, endereco, telefone, senha);
+                try {
+                    Integer id = DAO.getUsuarioDAO().create(usuario);
+                    erroRegistro.setText("Registro efetuado! Seu LOGIN DE ACESSO É: " + id);
+                } catch (Exception e) {
+                    erroRegistro.setText("Erro ao cadastrar. Tente novamente!");
+                    System.out.println(e);
+                }
+            }
+            else{
+
+                alertText.setText("Preencha os campos!");
+
+            }
+        }
+        else{
+
+            alertText.setText("Senha inválida! Máximo de 8 caracteres.");
+            senhaRegistroText.clear();
+
+        }
+
+
+        }
+
+
+    @FXML
+    void botaoVoltarAction(ActionEvent event) {
+
+        nomeText.clear();
+        enderecoText.clear();
+        teltext.clear();
+        senhaRegistroText.clear();
+
+        alertText.setText("");
+        erroRegistro.setText("");
+
+        HelloController.isAdm = false;
+        HelloController.isLei = false;
+        HelloController.isBib = false;
+        HelloApplication.changeScene("hello");
+    }
+
+    @FXML
+    void initialize() {
+        assert alertText != null : "fx:id=\"alertText\" was not injected: check your FXML file 'registro-view.fxml'.";
+        assert botaoCadastro != null : "fx:id=\"botaoCadastro\" was not injected: check your FXML file 'registro-view.fxml'.";
+        assert enderecoText != null : "fx:id=\"enderecoText\" was not injected: check your FXML file 'registro-view.fxml'.";
+        assert nomeText != null : "fx:id=\"nomeText\" was not injected: check your FXML file 'registro-view.fxml'.";
+        assert senhaRegistroText != null : "fx:id=\"senhaRegistroText\" was not injected: check your FXML file 'registro-view.fxml'.";
+        assert teltext != null : "fx:id=\"teltext\" was not injected: check your FXML file 'registro-view.fxml'.";
+        assert erroRegistro != null : "fx:id=\"erroRegistro\" was not injected: check your FXML file 'registro-view.fxml'.";
+        assert botaoVoltar != null : "fx:id=\"botaoVoltar\" was not injected: check your FXML file 'registro-view.fxml'.";
+    }
+
+}
